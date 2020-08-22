a4b38f77de569c3dd93b4ccb8aa8caa8dcd2072f
matteomartins <matteomcamargo@gmail.com>
2020-08-22 17:40:20 -0300

    pagina home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
-import {KaUser, KaCalendar} from '../../assets/icons';
+import {KaUser, KaCalendar, KaAddButton} from '../../assets/icons';
 import {KaCircleOutline, KaCircleSelected, KaArrow} from '../../assets/icons';
 
 import { useTheme } from '../../context/Theme';
@@ -12,9 +12,42 @@ import { IconTree, IconType } from '../../assets/icons/lib/esm';
 function Home(){
     return(
         <div className="main-home">
-            <div className="background">
-                
-            </div>
+            <div className="display-flex">
+                <div className="background">
+                    <div className="name">
+                        <h1>Etec de Taboão Da Serra</h1>
+                    </div>
+                    <div className="add-icon">
+                        <KaAddButton size={32} />  
+                    </div>
+                    <section id="classes">
+                        <article className="text">
+                            <h1 className="item">1A</h1>
+                            <span className="span">1° ANO A</span>
+                        </article>
+                        <article className="text">
+                            <h1 className="item">1B</h1>
+                            <span className="span">1° ANO B</span>
+                        </article>
+                        <article className="text">
+                            <h1 className="item">2A</h1>
+                            <span className="span">2° ANO A</span>
+                        </article>
+                        <article className="text">
+                            <h1 className="item">2B</h1>
+                            <span className="span">2° ANO B</span>
+                        </article>
+                        <article className="text">
+                            <h1 className="item">3A</h1>
+                            <span className="span">3° ANO A</span>
+                        </article>
+                        <article className="text">
+                            <h1 className="item">3B</h1>
+                            <span className="span">3° ANO B</span>
+                        </article>
+                    </section>
+                </div>
+            </div>  
             <Link to="/" className="calendar-content">
                 <div className="calendar-icon">
                     <KaCalendar size={22} color="var(--color-text-primary)" />  
