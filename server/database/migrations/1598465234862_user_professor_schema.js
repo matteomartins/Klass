49222fd1e7dbf5edf99b8c77a6480ae639b8bec0
Vitor Cavalcante <v9cavalcante@gmail.com>
2020-08-27 14:11:55 -0300

    Mais uma migration

---
+++ server/database/migrations/1598465234862_user_professor_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserProfessorSchema extends Schema {
+  up () {
+    this.create('user_professors', (table) => {
+      table.integer('user_id').notNullable()
+        .references('id').inTable('users')
+        .onDelete('CASCADE').onUpdate('CASCADE')
+      table.integer('professor_id').notNullable()
+        .references('id').inTable('professors')
+        .onDelete('CASCADE').onUpdate('CASCADE')
+    })
+  }
+
+  down () {
+    this.drop('user_professors')
+  }
+}
+
+module.exports = UserProfessorSchema
